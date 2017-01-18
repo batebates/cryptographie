db0c000bf85b6abb86cbe065da996146b554cf72
batebates <baptiste.decrand@laposte.net>
2017-01-18 19:25:10 +0100

    add snippet

---
+++ Cryptographie/faible/cesar/Test.rb
@@ -0,0 +1,27 @@
+#<b>Auteur  :</b> Decrand Baptiste
+#
+#<b>Version :</b>
+#
+#<b>Date    :</b>
+#
+#===butdelaclasse
+#<b>Liste des méthodes
+#*
+#</b>
+class
+  #== Variable de classe ==
+
+  #===========================
+  #== Variables d'instance ==
+
+  #===========================
+
+  private_class_method :new
+
+  def initialize()#:nodoc:
+  end
+  def .creer()
+    new()
+  end
+
+end
